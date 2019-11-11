bc044d05cd7bfa40161afc2da34a27c1105d4634
Daniel Koch <daniel.p.koch@gmail.com>
2019-11-11 10:05:19 -0700

    Log stdout and stderr from subprocess calls to log file

--- gwmh/job.py
+++ gwmh/job.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 import subprocess
 
+import datetime
 import logging
 
 def mkdocs_job(repository, branch, output_path):
@@ -10,6 +11,12 @@ def mkdocs_job(repository, branch, output_path):
                         level=logging.DEBUG)
 
     logging.info("Starting MkDocs build job")
+
+    stdoutfile = open('logs/mkdocs_job_std.log', 'w')
+    stdoutfile.write('='*80 + '\n')
+    stdoutfile.write('Starting MkDocs build job, {}\n'.format(datetime.datetime.now()))
+    stdoutfile.write('='*80 + '\n')
+    stdoutfile.flush()
 
     repo_name = repository.split("/")[-1]
 
@@ -22,34 +29,40 @@ def mkdocs_job(repository, branch, output_path):
 
     logging.info("Cloning repository %s from GitHub, %s branch", repository, branch)
     result = subprocess.run(
-        ['git', 'clone', '--recursive', '-b', branch, 'https://github.com/{}.git'.format(repository)], cwd=tmpdir)
+        ['git', 'clone', '--recursive', '-b', branch, 'https://github.com/{}.git'.format(repository)],
+        cwd=tmpdir, stdout=stdoutfile, stderr=subprocess.STDOUT)
     if not result.returncode == 0:
         logging.error("Failed to clone repository %s, %s branch. Aborting.", repository, branch)
         return False
 
     logging.info("Building mkdocs site")
     result = subprocess.run(
-        ['mkdocs', 'build', '-d', '../site'], cwd='{}/{}'.format(tmpdir, repo_name))
+        ['mkdocs', 'build', '-d', '../site'], cwd='{}/{}'.format(tmpdir, repo_name), stdout=stdoutfile, stderr=subprocess.STDOUT)
     if not result.returncode == 0:
         logging.error("Failed to build mkdocs site. Aborting.")
         return Falses
 
     if os.listdir(output_path):
         logging.info("Deleting contents of output directory %s", output_path)
-        result = subprocess.run('rm -r {}/*'.format(output_path), shell=True)
+        result = subprocess.run('rm -r {}/*'.format(output_path), shell=True, stdout=stdoutfile, stderr=subprocess.STDOUT)
         if not result.returncode == 0:
             logging.error("Failed to delete contents of output directory %s", output_path)
             return False
 
     logging.info("Copying mkdocs site to output directory %s", output_path)
     result = subprocess.run('cp -r * {}'.format(output_path),
-                            cwd='{}/site'.format(tmpdir), shell=True)
+                            cwd='{}/site'.format(tmpdir), shell=True, stdout=stdoutfile, stderr=subprocess.STDOUT)
     if not result.returncode == 0:
         logging.error("Failed to copy mkdocs site to output directory %s", output_path)
         return False
 
     logging.info("Deleting temporary working directory %s", tmpdir)
-    subprocess.run(['rm', '-rf', tmpdir])
+    subprocess.run(['rm', '-rf', tmpdir], stdout=stdoutfile, stderr=subprocess.STDOUT)
+
+    stdoutfile.write('='*80 + '\n')
+    stdoutfile.write('Job Successful\n')
+    stdoutfile.write('='*80 + '\n')
+    stdoutfile.close()
 
     logging.info("Mkdocs build job completed succesfully!")
 
